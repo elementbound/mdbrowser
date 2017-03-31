1758cb01b5f28bd29b453eb5bdad499359719733
Gálffy Tamás <ezittgtx@gmail.com>
2017-03-31 14:23:46 +0300

    Quick browse template test

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import flask
 
+def get_drives():
+    import string
+    import os.path
+
+    return [d+':' for d in string.ascii_uppercase if os.path.exists(d+':')]
+
 app = flask.Flask('mdbrowser')
 
 @app.route('/')
 def index():
-    return flask.render_template('index.html')
+    b = flask.Markup(browse(''))
+    return flask.render_template('index.html', browse_content=b)
 
 @app.route('/browse/<path:at>')
 def browse(at):
-    return 'Files, dam'
+    drives = get_drives()
+    return flask.render_template('browse.html', drives=drives)
 
 @app.route('/render/<path:file>')
 def render(file):
