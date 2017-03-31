6e480a2f3183e8e1e8a103b83323fc0656039547
Gálffy Tamás <ezittgtx@gmail.com>
2017-03-31 15:41:10 +0300

    Can now recursively list directories

--- js/site.js
+++ js/site.js
@@ -20,9 +20,56 @@ function formatEntry(entry) {
     let text = $('<a>').appendTo(cell);
     text.html(entry.name);
 
-    row.attr('data-path', entry.path);
+    row.data('entry', entry);
 
     return row;
+}
+
+function toggleDirectory(cell) {
+    var parent = cell.parent();
+    var entry = parent.data('entry');
+
+    if(cell.prop('expanded')) {
+        // Collapse cell
+        let children = cell.data('children');
+        for(let i = 0; i < children.length; i++)
+            children[i].remove();
+
+        // Remember
+        cell.removeProp('expanded');
+
+        // Show
+        cell.find('.icon-expand')
+            .removeClass('glyphicon-minus')
+            .addClass('glyphicon-plus');
+    } else {
+        var children = [];
+
+        // Expand tree
+        $.getJSON('/list/'+entry.path, undefined, function(data) {
+            // Reverse-iterate entries
+            for(let i = data.length; i--; ) {
+                let entry = data[i];
+                let row = formatEntry(entry);
+                let cell = row.find('td');
+
+                row.insertAfter(parent);
+                children.push(row);
+
+                if(entry.flags.directory)
+                    cell.click(function(){toggleDirectory($(this));});
+            }
+        });
+
+        // Remember
+        cell.prop('expanded', true);
+        cell.data('children', children);
+
+        // Show
+        cell.find('.icon-expand')
+            .removeClass('glyphicon-plus')
+            .addClass('glyphicon-minus');
+    }
 }
 
 $(document).ready(function() {
@@ -38,36 +85,7 @@ $(document).ready(function() {
 
             row.appendTo(body);
 
-            cell.click(function() {
-                var cell = $(this);
-                var parent = cell.parent();
-
-                if(cell.prop('expanded')) {
-                    // Collapse cell
-                    cell.data('children').remove();
-
-                    // Remember
-                    cell.removeProp('expanded');
-                } else {
-                    var children = [];
-
-                    // Expand tree
-                    $.getJSON('/list/'+entry.path, undefined, function(data) {
-                        // Reverse-iterate entries
-                        for(let i = data.length; i--; ) {
-                            let entry = data[i];
-                            let row = formatEntry(entry);
-                            let cell = row.find('td');
-
-                            row.insertAfter(parent);
-                            children.push(row);
-                        }
-                    });
-
-                    cell.prop('expanded', true);
-                    cell.data('children', children);
-                }
-            })
+            cell.click(function(){toggleDirectory($(this));});
         }
     })
 })
